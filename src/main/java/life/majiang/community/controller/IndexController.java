c15b976a8d65cbc5041d46942c2b0cd6ab629f5b
kange2001 <kange2001@189.cn>
2020-12-13 20:53:15 +0800

    add index

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+/*
 @Controller
 public class HelloController {
 
@@ -13,4 +13,12 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello";
     }
+}
+
+ */
+
+@Controller
+public class IndexController{
+    @GetMapping("/")
+    public String index(){ return "index";}
 }
